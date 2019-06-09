17513f3edda7e64b8973c6057cfac74e7a2d2a51
ChernetsovNG <n.chernetsov86@gmail.com>
2019-06-09 13:06:15 +0300

    new URL, view and template to display lists

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -6,7 +6,11 @@ def home_page(request):
     '''домашная страница'''
     if request.method == 'POST':
         Item.objects.create(text=request.POST['item_text'])
-        return redirect('/')
+        return redirect('/lists/one-unique-list-in-the-world/')
+    return render(request, 'home.html')
 
+
+def view_list(request):
+    '''представление списка'''
     items = Item.objects.all()
-    return render(request, 'home.html', {'items': items})
+    return render(request, 'list.html', {'items': items})
