6ceac899f003c5006d7977465a91b5defb103da5
ChernetsovNG <n.chernetsov86@gmail.com>
2019-06-09 13:30:43 +0300

    Элементы связаны с разными списками

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import redirect, render
-from lists.models import Item
+from lists.models import Item, List
 
 
 def home_page(request):
@@ -9,7 +9,8 @@ def home_page(request):
 
 def new_list(request):
     '''новый список'''
-    Item.objects.create(text=request.POST['item_text'])
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect('/lists/one-unique-list-in-the-world/')
 
 
